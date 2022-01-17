bc1bd80f4abee90a68d109904e97dd74547725ec
Tiago Silva <a93277@alunos.uminho.pt>
2022-01-17 23:22:14 +0000

    começo da magia

---
+++ Localizarte/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Localizarte.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
