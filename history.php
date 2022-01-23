73135355100abcfc6c5723927278d8d54b8b5efa
realRunlo <gteixeira.pereira@gmail.com>
2022-01-23 01:00:08 +0000

    history

--- history.php
+++ history.php
@@ -2,11 +2,13 @@
 require_once("connectDB.php");
 
 if(isSet($_SESSION['username'])){
-  if(isSet($_SESSION['loginErro']) && $_SESSION['loginErro'] == 0){
-
 //get 5 last visited places order by datetime
-$result = mysqli_query($connection,"SELECT museum FROM history WHERE username='".$_SESSION['username']."' ORDER BY datetime DESC LIMIT 5 ");
+
+$query = "SELECT museum FROM history WHERE username='" . $_SESSION['username'] . "' ORDER BY datetime DESC LIMIT 5 ";
+$result = mysqli_query($connection,$query);
+$rowNumb = mysqli_num_rows($result);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -14,10 +16,13 @@ $result = mysqli_query($connection,"SELECT museum FROM history WHERE username='"
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	  <link rel="stylesheet" type="text/css" href="css/searchPage.css">
-    <link href="css/default.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/default.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/table.css">
     <style>
     body {font-family: "Lato", sans-serif}
     .mySlides {display: none}
@@ -47,28 +52,64 @@ $result = mysqli_query($connection,"SELECT museum FROM history WHERE username='"
       </div>
     </div>
 
-    <div class="container">
-       <div class="container-table">
-         <table>
-           <?php
-              while(($row = mysqli_fetch_assoc($result)) ){
-                $textName = ucfirst($row["museum"]);
-                $refName = str_replace(' ', '-', $row["museum"]);
-                echo "<tr>";
-                echo "<td class=title><a href=museum/museum.php?name=".$refName .">".$textName." </a> </td>";
-                echo "</tr>";
-              }?>
+  	<section class="ftco-section">
+  		<div class="container">
+  			<div class="row justify-content-center">
+  				<div class="col-md-6 text-center mb-5">
+  				</div>
+  			</div>
+  			<div class="row">
+  				<div class="col-md-12">
+  					<div>
+  						<table class="table table-bordered table-hover">
+  						  <thead class="thead-custom" >
+                  <th class="align-center">Museum</th>
+                  <th class="align-center">Date</th>
+  						  </thead>
 
-          </table>
+  						  <tbody class="table-custom">
+                  <?php
+                   if($rowNumb>0){
+                        while($row = mysqli_fetch_assoc($result)){
+                            $textName = ucfirst($row["museum"]);
+                            $date = date('Y-m-d H:i:s',$row['datetime']);
+                            $refName = str_replace(' ', '-', $row["museum"]);
+                            $url = "museum/museum.php?name=".$refName;
+                            ?>
+                            <tr  onclick="window.location.assign('<?php echo $url; ?>')">
+                              <td class="align-center"><?php echo $textName ?> </td>
+                              <td class="align-center"> <?php echo $date; ?> </td>
+                            </tr>
+                            <?php
+                          }
+                   }else{
+                     ?>
+                          <td>Empty.</td>
+                          <td></td>
+               <?php
+                     }
+                  ?>
+  						      <!--<th scope="row">1</th>-->
+  						  </tbody>
+  						</table>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  	</section>
 
-        </div>
+  	<script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 
-    </div>
+  	</body>
+
+
 
   </body>
 </html>
-
-<?php }
+<?php
 }
-else header('Location: http://localhost/LI4/login.php');
+  else header('Location: http://localhost:8888/login.php');
 ?>
